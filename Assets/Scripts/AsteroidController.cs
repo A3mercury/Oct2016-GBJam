0e12a4085f3bacab8e1afc9986152135000da923
Austin Andrews <a5mercuryhu@gmail.com>
2016-10-01 11:14:36 -0500

    Add initial project - closes #1, closes #2, closes #4

---
+++ Assets/Scripts/AsteroidController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AsteroidController : MonoBehaviour {
+
+	public PlayerController player;
+
+	public float asteroidGravity;
+	public float distanceTreshold;
+
+	private Vector2 asteroidSize;
+	
+	public Sprite[] asteroidSprites = new Sprite[9];
+
+	private float spriteSize = 8f;
+	
+	
+	// Use this for initialization
+	void Start () {
+	
+		asteroidSize = new Vector2(3, 3);
+		
+		asteroidGravity = .0025f;
+		distanceTreshold = 10f;
+		
+		SpawnAsteroid();
+			
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Vector2.Distance(player.transform.position, transform.position) < distanceTreshold) {
+			player.AddGravity(transform.position, asteroidGravity);
+		}
+	}
+	
+	void SpawnAsteroid() {
+
+		for (var i = -1; i <= 1; i++) {
+			for (var j = -1; j <= 1; j++) {
+				// get midpoint of asteroid size vector
+				int midX = Mathf.RoundToInt(asteroidSize.x / 2);
+				int midY = Mathf.RoundToInt(asteroidSize.y / 2);
+				
+				GameObject go = new GameObject();
+				SpriteRenderer ren = go.AddComponent<SpriteRenderer>() as SpriteRenderer;
+				ren.sprite = asteroidSprites[4];
+				go.name = "ast-spr";
+				go.transform.position = new Vector2(transform.position.x + (i * spriteSize), transform.position.y + (j * spriteSize));
+				go.transform.parent = transform;
+				go.AddComponent<BoxCollider2D>();
+				Rigidbody2D rb = go.AddComponent<Rigidbody2D>();
+				rb.gravityScale = 0;
+				rb.mass = 10000;
+				rb.fixedAngle = true;
+			}
+		}
+						
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float jetPackSpeed = 50f;
+	private float rotateSpeed = 10f;
+	
+	private Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+		playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float vertical = Input.GetAxisRaw("Vertical");
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float shift = Input.GetAxisRaw("Shift");
+		
+		playerRigidBody.AddRelativeForce(Vector2.up * vertical * jetPackSpeed, ForceMode2D.Force);
+		
+		if (shift > 0) {
+			playerRigidBody.AddRelativeForce(Vector2.right * horizontal * jetPackSpeed, ForceMode2D.Force);
+		} else {
+			playerRigidBody.AddTorque(-(horizontal) * rotateSpeed);
+		}
+
+	}
+	
+	public void AddGravity(Vector3 asteroidPosition, float gravity) {
+		Vector2 direction = transform.position + asteroidPosition;
+		playerRigidBody.AddForce(direction * gravity, ForceMode2D.Force);
+	}
+}
